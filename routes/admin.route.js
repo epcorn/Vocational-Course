f004988ad78604844241172dbde6e06390b18ff1
epcorn <exteam.epcorn@gmail.com>
2024-02-19 16:20:58 +0530

    removed signup route for admins

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { login, signout, signup, anylytics, demography, documentUpload } from "../controllers/admin.controller.js";
+import { login, signout, anylytics, demography, documentUpload } from "../controllers/admin.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signout", signout);
-router.post("/signup", signup); //Remove this route before movig this project to production.
 router.get("/anylytics", verifyToken, anylytics);
 router.get("/demography", verifyToken, demography);
 router.post("/documentUpload", verifyToken, documentUpload);
