6dc5ddae41ca3a459045caf540dec1c4aec8f43c
epcorn <exteam.epcorn@gmail.com>
2024-04-29 17:41:07 +0530

    modified logo component

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,12 +1,15 @@
-import logo from '../images/logo1.png'
-import logo1 from '../images/logo2.png'
+import logo from "../images/logo1.png";
+import logo1 from "../images/logo2.png";
 
 export function Logo(props) {
   const { width1 = "w-20", width2 = "w-40" } = props;
 
   return (
-    <div className='flex'>
-      <img src={logo} alt="logo" className={width1} />
+    <div className="flex">
+      <div className=" flex-col">
+        <img src={logo} alt="logo" className={width1} />
+        <span>Serampore College</span>
+      </div>
       <img src={logo1} alt="logo" className={width2} />
     </div>
   );
