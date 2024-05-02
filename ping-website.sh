a6359e423b6be40d08637e77282dcdd78ee19eba
epcorn <exteam.epcorn@gmail.com>
2024-05-02 15:55:13 +0530

    Add website pinging action

--- ping-website.sh
+++ ping-website.sh
@@ -9,7 +9,6 @@ PING_RESULT=$(curl --write-out '%{http_code}' --silent --output /dev/null "$WEBS
 # Check the response code
 if [ "$PING_RESULT" -eq 200 ]; then
     echo "Website is up and running!"
-    echo $PING_RESULT
 else
     echo "Error: Website is down or not responding correctly. Response code: $PING_RESULT"
 fi
